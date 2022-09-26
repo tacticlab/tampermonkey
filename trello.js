200dc3188f55ad81e4b2e3fe6809ba5fbe62917e
tacticlab <tacticlab@users.noreply.github.com>
2022-09-26 13:44:35 +1000

    Add basic style

--- trello.js
+++ trello.js
@@ -1,1 +1,5 @@
-console.log("Trello.js loaded");
+let customCSS = document.createElement('style');
+customCSS.innerHTML = `
+  .js-card-cover-chooser {display:none !important}
+`;
+document.head.appendChild(customCSS);
